9c013b20b798d2986efffde872f487cb28952be7
Winged-One <kruglovaelizaveta1@gmail.com>
2021-06-20 15:12:16 +0600

    Добавила список #12

--- main.py
+++ main.py
@@ -23,6 +23,9 @@ IssueTypesKeyboard.add('Заброшеные объекты', 'Детские п
 IssueTypesKeyboard.add('Доступная среда', 'Другое')
 hideBoard = telebot.types.ReplyKeyboardRemove()
 
+TypeInputConstraint1 = ['Тратуары', 'Пешеходные переходы', 'Освещение', 'Автомобильные  дороги', 'Ливневая канализация']
+TypeInputConstraint2 = ['Заброшеные объекты', 'Детские площадки', 'Свалки', 'Проведение капитального ремонта']
+TypeInputConstraint3 = ['Доступная среда', 'Другое']
 
 def logger_init():
     log = logging.getLogger("BKG_BOT")
@@ -115,7 +118,12 @@ def main_handler(message: telebot.types.Message):
         return
 
     if user.state == 'state_ask_photo':
-        if message.text != '':
+        if message.text in TypeInputConstraint1 or message.text in TypeInputConstraint2:
+            user.issue.type = message.text
+            bot.send_message(user.id, 'Отправьте фото')
+            user.state = 'state_ask_geo_or_address'
+            return
+        elif message.text in TypeInputConstraint3:
             user.issue.type = message.text
             bot.send_message(user.id, 'Отправьте фото')
             user.state = 'state_ask_geo_or_address'
@@ -123,6 +131,11 @@ def main_handler(message: telebot.types.Message):
         else:
             user.state = 'state_ask_type'
             main_handler(message)
+
+    if user.state == 'state_dumb_user_1':
+        bot.send_message(user.id, 'Отправьте фото')
+        user.state = 'state_ask_geo_or_address'
+        return
 
     if user.state == 'state_ask_geo_or_address':
         if message.content_type == 'photo':
@@ -138,37 +151,42 @@ def main_handler(message: telebot.types.Message):
             user.state = 'state_ask_description'
             return
         else:
-            user.state = 'state_ask_photo'
+            user.state = 'state_dumb_user_1'
             main_handler(message)
+
+    if user.state == 'state_dumb_user_2':
+        bot.send_message(user.id, 'Отправьте геопозицию (желательно) или адрес')
+        user.state = 'state_ask_description'
+        return
 
     if user.state == 'state_ask_description':
         if message.text != '' or message.content_type == 'location':
             if message.content_type == 'location':
                 # Преобразование в удобный формат для работы с картами
                 user.issue.geo = str(message.location.latitude) + ',' + str(message.location.longitude)
-            else:
+            elif message.content_type != 'photo':
                 user.issue.address = message.text
             bot.send_message(user.id, 'Отправьте описание')
             user.state = 'state_create_issue'
             return
         else:
-            user.state = 'state_ask_geo_or_address'
+            user.state = 'state_dumb_user_2'
             main_handler(message)
 
-
     if user.state == 'state_create_issue':
-        user.issue.description = message.text
-        filepath: str = settings.output_files_directory + user.issue.type + "_" + str(
-            user.issue.send_time.timestamp()) + "_" + str(
-            user.id) + ".yaml"
-        save_issue_to_yaml(filepath, user.issue)
-        bot.send_message(user.id, 'Обращение успешно сохранено').wait()
-        bot.send_message(user.id, 'Спасибо, за то, что отправили обращение')
-        bot.send_message(user.id, 'Вместе мы сможем сделать город комфортнее и безопаснее')
-        logger.info(f"New issue saved to {filepath} by user: {user.id}")
-        user.state = 'init'
-        main_handler(message)
-        user.reset_issue()
+        if message.content_type != 'photo':
+            user.issue.description = message.text
+            filepath: str = settings.output_files_directory + user.issue.type + "_" + str(
+                user.issue.send_time.timestamp()) + "_" + str(
+                user.id) + ".yaml"
+            save_issue_to_yaml(filepath, user.issue)
+            bot.send_message(user.id, 'Обращение успешно сохранено').wait()
+            bot.send_message(user.id, 'Спасибо, за то, что отправили обращение')
+            bot.send_message(user.id, 'Вместе мы сможем сделать город комфортнее и безопаснее')
+            logger.info(f"New issue saved to {filepath} by user: {user.id}")
+            user.state = 'init'
+            main_handler(message)
+            user.reset_issue()
 
 # Сохранения изображения на диск
 def save_image(file_id, image_filepath):
